c85f1a57fcd5e4d6729c2a82669daaaa4aa06b7a
ackimball <andreackimball@gmail.com>
2016-04-23 17:13:26 -0700

    simplifying

--- azureconfig.sh
+++ azureconfig.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-if [ $# -ne 2 ]; then
-    echo "Please provide your storage account name and key"
-    echo "./azureconfig.sh [storage name] [account key]"
-    exit 1
-fi
 
 cat > config.py << EOF
 STORAGE_ACCOUNT_NAME = '$NAME'
